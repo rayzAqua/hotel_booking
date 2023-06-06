a8b8e7dfb3ae0436b5d13478406cb82e2791fef0
rayzAqua <hieulcgl@gmail.com>
2023-06-06 18:08:15 +0700

    "Clean comment"

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,18 +4,6 @@ import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express();
 
-// router.get("/checkauthentication", verifyToken, (req, res, next) => {
-//     res.send("You are logged in.")
-// });
-
-// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-//     res.send("You are user, user can delete user's acccount.")
-// });
-
-// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
-//     res.send("You are admin, admin can do everything.")
-// });
-
 // UPDATE
 router.put("/:id", verifyUser, updateUser);
 
